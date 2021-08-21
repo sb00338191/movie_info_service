ab2ac943c9205b12a786e2e46b3b5f601181ad9d
Soumen Bose <soumen.bose11@gmail.com>
2021-08-21 09:01:12 +0530

    Add files via upload

---
+++ src/test/java/com/soumen/movieinfoservice/MovieinfoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumen.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieinfoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
